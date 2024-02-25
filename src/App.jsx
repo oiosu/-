7a89b601aabfac4262319c00a97d2bf3eb68a863
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-02-25 16:15:49 +0900

    feat: 파비콘 수정 및 title 수정

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-no-undef */
+import "./App.css";
+import "./Components/MainPage/HomePage.jsx";
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MainPage/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <div>
+        <p>착한 부동산 거래</p>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
